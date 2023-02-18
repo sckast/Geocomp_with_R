520278a74c503b414396bc6be5ceee1f9e477a9b
Sophia Kast <112989844+sckast@users.noreply.github.com>
2023-02-18 15:46:15 -0800

    Continuing work on Ch 3 exercises

--- Scripts/Geocomp-w-R_Ch3_exercises.R
+++ Scripts/Geocomp-w-R_Ch3_exercises.R
@@ -63,3 +63,75 @@ west_states#check whole df because it's small anyway
 us_states$AREA <- units::set_units(us_states$AREA, km^2)
 us_states$AREA#make sure that it worked as intended
 class(us_states$AREA)#class is back to 'units'
+
+#find all states that belong to the south region, have an area larger than 
+#150000 km^2 OR a total population in 2015 larger than 7,000,000
+us_states$AREA <- as.numeric(us_states$AREA)#remove units from AREA column
+#subset
+south_states <- us_states %>% 
+  filter(REGION == "South" &
+           (AREA > 150000 | total_pop_15 > 7000000))
+#set units back for the subset and the full US df
+south_states$AREA <- units::set_units(south_states$AREA, km^2)
+us_states$AREA <- units::set_units(us_states$AREA, km^2)
+
+#exercise 4
+#what was the total population in 2015 in the us_states dataset?
+total_pop <- sum(us_states$total_pop_15)
+total_pop#314,375,347 people
+
+#what was the max and min total pop in 2015?
+min(us_states$total_pop_15)#579,679 people
+max(us_states$total_pop_15)#38,421,464 people
+
+#exercise 5
+#how many states are there in each region
+num_states <- us_states %>% 
+  select(REGION, NAME) %>% #subset the columns
+  group_by(REGION) %>% #group by region
+  summarize(NUM_STATES = n()) %>% #get the number of states in each region
+  mutate(NUM_STATES) #add a column with the number of states in each region
+num_states 
+
+#exercise 6
+#What was the minimum and maximum total population in 2015 in each region? 
+pop_region <- us_states %>% 
+  select(REGION, total_pop_15) %>% #subset the columns I need
+  group_by(REGION) %>% #group by region
+  #get the min and max pops
+  summarize(MIN_POP = min(total_pop_15), MAX_POP = max(total_pop_15)) %>%
+  mutate(MIN_POP = MIN_POP, MAX_POP = MAX_POP)
+pop_region
+
+#What was the total population in 2015 in each region?
+total_pop_region <- us_states %>% 
+  select(REGION, total_pop_15) %>% #subset the columns I need
+  group_by(REGION) %>% #group by region
+  summarize(pop = sum(total_pop_15)) %>% #get the sum of the pops in each region
+  mutate(TOTAL_POP = pop) #add a column with the total pop for each region
+total_pop_region              
+
+#exercise 7
+#Add variables from us_states_df to us_states, 
+#and create a new object called us_states_stats. 
+names(us_states_df)
+names(us_states)
+us_states_stats <- left_join(us_states_df, us_states, 
+                             by = c(state = "NAME"))
+us_states_stats
+#What function did you use and why? 
+#I used left join and put the us_states_df first in order to preserve all 51 
+#observations even though 2 are missing from us_states
+
+#Which variable is the key in both datasets? 
+#'NAME' in us_states and 'state' in us_states_df
+
+#What is the class of the new object?
+class(us_states_stats) #tibble and dataframe. no longer an sf object. 
+
+#exercise 8
+#us_states_df has two more rows than us_states. 
+#How can you find them? (hint: try to use the dplyr::anti_join() function)
+us_states_df <- rename(us_states_df, NAME = state)
+anti_join(us_states, us_states_df, 
+          by = "NAME")
